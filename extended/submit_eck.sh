2f213b700cd769af1dc48aaeaa93c2b4da3c91f0
MariaSanchezVivo <m.sanchezvivo@um.es>
2026-03-23 01:15:59 +0100

    Subida carpeta extended (parte opcional)

---
+++ extended/submit_eck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH -p eck-q
+#SBATCH --chdir=/home/alumno23/MM/for-students/TAREA_PERFECTA/extended
+#SBATCH -J equilibrado_extended
+#SBATCH --cpus-per-task=1
+
+date
+gmx mdrun -deffnm are -c are.g96 -nt 1
+date
+
+
+
